56305000b44b66c13aaf97c588407ac3eb62a671
Nicholas Lambert <nick@thedesigniac.com>
2016-09-12 13:18:05 -0600

    DayOneOfQueries
    Practiced basic queries
    Went over sever parameters

--- FirstQueries.sql
+++ FirstQueries.sql
@@ -1,3 +1,31 @@
 --Seperate columns with comas
+/*
 SELECT CustNo, CustFirstName, CustLastName, custstreet, custcity, custzip, custbal
-FROM Customer;
+FROM Customer
+WHERE Custcity = 'Seattle'
+AND CustLastName = 'Taylor';
+*/
+/*
+SELECT CustNo, CustFirstName, CustLastName, custstreet, custcity, custzip, custbal
+FROM Customer
+Where CustCity = 'Seattle'
+OR CustCity = 'Denver'
+ORDER BY CustNo;
+*/
+/*
+SELECT ProdNo, ProdName, Prodmfg, ProdPrice, ProdNextShipDate, ProdQOH
+FROM Product
+WHERE PRODPRICE > 100
+AND ProdNextShipDate > '01-Feb-07';
+*/
+/*
+SELECT ProdNo, ProdName AS Name, Prodmfg "Manufacturer", ProdPrice AS Price, ProdNextShipDate, ProdQOH
+FROM Product
+WHERE Prodmfg LIKE '%on%'
+AND ProdPrice BETWEEN 100 and 200;
+*/
+
+SELECT CustNo, CustFirstName, CustLastName, custstreet, custcity, custzip, custbal
+FROM Customer
+WHERE CustLastName LIKE 'S%'
+OR CustLastName LIKE 's%';
