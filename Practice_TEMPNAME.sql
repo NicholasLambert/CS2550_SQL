b18808d9ed56eddf78513cd536f3645c0c059cc7
Nicholas Lambert <nick@thedesigniac.com>
2016-10-03 12:41:33 -0600

    Edits to Practice - Created Assignment3

--- Practice_TEMPNAME.sql
+++ Practice_TEMPNAME.sql
@@ -68,3 +68,64 @@ ORDER BY "Day of the Month";
 --List the order number, order date and the number of days since the order (as of december 31, 2007). Sort by number of days since the order
 SELECT OrdNo, OrdDate, To_Char(To_Date('DEC 31, 2007', 'Mon DD, YYYY') - OrdDate, '999') AS "Day since ORder"
 FROM OrderTBL;
+
+--Selects the max, min and Average or ProdPrice
+--Max and Min will work on strings as well. AVG with through an error
+SELECT MAX(ProdPRice)
+From Product;
+
+SELECT AVG(ProdPrice)
+FROM Product;
+
+Select MIN(ProdPrice)
+FROM Product;
+
+--Count doesn't work on Null values. It will only count items with a value
+--Returns the count of a given set when using asterisk
+SELECT COUNT(ProdNextShipDate)
+FROM Product;
+
+SELECT COUNT(*)
+FROM Product;
+
+SELECT COUNT(*)
+FROM Customer;
+
+--Return the sum of everything in the column
+--This statement sorts by product manufacturer
+SELECT SUM(ProdPrice), SUM(ProdQOH), ProdMFG
+FROM Product
+GROUP BY ProdMFG;
+
+--Grouo by the Customer state while displaying the avg custBal
+--shorten digits down with To_Char
+SELECT To_Char(AVG(CustBal), '999.99') AS Customer_Balance_Per_State, CustSTate
+FROM Customer
+GROUP BY CustState;
+
+--gets the averages that are higher than 500. Having Statement Used
+SELECT To_Char(AVG(CustBal), '999.99') AS Customer_Balance_Per_State, CustCity, CustState
+FROM Customer
+GROUP BY CustCity, CustState
+HAVING AVG(CustBal) >= 500;
+
+--Get the total number of customers in each state
+SELECT COUNT(*), CustState
+FROM Customer
+GROUP BY CUSTSTATE;
+
+--gets the employee commission rates
+SELECT COUNT(*) AS NumEmployeesPerCommission, Empno, Empcommrate
+FROM Employee
+WHERE EmpCommRate IS NOT NULL
+GROUP BY EmpCommRate;
+
+--Working with number manipulation stuffs, I don't really remember
+SELECT To_Char((Sysdate - ProdNextShipDate)/365, '99.99') AS "Number of Years"
+FROM Product
+WHERE ProdNextShipDate IS NOT NULL
+Order By "Number of Years";
+
+--Getting rid of duplicate stuff
+SELECT To_Char(OrdDate, 'DD Mon YYYY HH:MI')
+FROM OrderTBL;
