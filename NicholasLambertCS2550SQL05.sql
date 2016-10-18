a85696e665da627c6faceff5fafb34ae81ad2495
Nicholas Lambert <nick@thedesigniac.com>
2016-10-17 20:01:04 -0600

    Waste of a commit

--- NicholasLambertCS2550SQL05.sql
+++ NicholasLambertCS2550SQL05.sql
@@ -1,3 +1,5 @@
 /* Nicholas Lambert */
 
 /* #1 - List the full name and phone number for students that live in Newark, NJ. Sort on last name and first name */
+SELECT first_name, last_name, phone
+FROM 
